0caccb1e7acc7a400399294b5cee520c5d350e28
Connor Van Elswyk <ConnorVanElswyk@gmail.com>
2019-05-20 14:49:00 -0400

    initial commit

---
+++ src/main/java/Handler.java
@@ -0,0 +1,14 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Request, String> {
+
+
+
+	@Override
+	public String handleRequest(Request input, Context context) {
+		return null;
+	}
+
+
+}
---
+++ src/main/java/Request.java
@@ -0,0 +1,9 @@
+import lombok.Data;
+
+@Data
+public class Request {
+
+	private String proxyHostName;
+	private Integer proxyHostPort;
+
+}
